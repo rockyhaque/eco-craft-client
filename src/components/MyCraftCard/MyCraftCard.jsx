59124d78fcd761fde1196ffa547ae2e3048574e0
Rocky Haque <rockyhaque99@gmail.com>
2024-06-25 10:53:10 +0600

    myCraft section done

--- src/components/MyCraftCard/MyCraftCard.jsx
+++ src/components/MyCraftCard/MyCraftCard.jsx
@@ -3,11 +3,11 @@ import { IoIosStar } from "react-icons/io";
 import { BiCategoryAlt } from "react-icons/bi";
 import { IoTimeOutline } from "react-icons/io5";
 import { MdOutlineDesignServices } from "react-icons/md";
+import PropTypes from 'prop-types';
+
 
 const MyCraftCard = ({ craft }) => {
-  console.log(craft);
   const {
-    _id,
     name,
     price,
     category,
@@ -17,83 +17,92 @@ const MyCraftCard = ({ craft }) => {
     stockStatus,
     customization,
   } = craft;
+
   return (
-    <div>
-      {/* component */}
-      <div className="flex min-h-screen items-center justify-center bg-gray-100 ">
-        <div className="flex font-sans">
-          <div className="flex-none w-48 relative">
-            <img
-              src={craftPhotoURL}
-              alt="Craft Photo"
-              className="absolute inset-0 w-full h-full object-cover rounded-xl"
-              loading="lazy"
-            />
+    <div className="border p-2 rounded-xl shadow-md w-full ">
+      <div className="flex flex-col md:flex-row">
+        <div className="flex-none w-full md:w-48 relative mb-4 md:mb-0">
+          <img
+            src={craftPhotoURL}
+            alt="Craft Photo"
+            className="w-full h-full object-cover rounded-xl"
+          />
+        </div>
+        <form className="flex-auto p-4">
+          <div className="flex flex-wrap">
+            <h1 className="flex-auto text-xl font-semibold text-gray-900">
+              {name}
+            </h1>
+            <div className="flex justify-center items-center gap-2 text-lg font-semibold text-black-500">
+              <FaBangladeshiTakaSign />
+              {price}
+            </div>
+            <div className="w-full flex-none text-sm font-medium text-black-700 mt-2">
+              {stockStatus}
+            </div>
           </div>
-          <form className="flex-auto p-6">
-            <div className="flex flex-wrap">
-              <h1 className="flex-auto text-xl font-semibold text-gray-900">
-                {name}
-              </h1>
-              <div className="flex justify-center items-center gap-2 text-lg font-semibold text-black-500">
-                <span>
-                  <FaBangladeshiTakaSign />
-                </span>{" "}
-                {price}
-              </div>
-              <div className="w-full flex-none text-sm font-medium text-black-700 mt-2">
-                {stockStatus}
-              </div>
+          <div className="mt-4 mb-6 pb-6 border-b border-slate-200">
+            <div className="flex items-center gap-3">
+              <BiCategoryAlt />
+              <span className="bg-teal-300 rounded-full px-4 ">
+                {category}
+              </span>
             </div>
-            <div className="mt-4 mb-6 pb-6 border-b border-slate-200">
-              <div className="flex items-center gap-3">
-                <BiCategoryAlt />
-                <span className="bg-teal-300 rounded-full px-4 ">
-                  {category}
-                </span>
-              </div>
-              <div className="flex items-center gap-3 mt-2">
-                <IoTimeOutline />
-                <span className="bg-gray-300 rounded-md px-4 ">
-                  {processing_time}
-                </span>
-              </div>
+            <div className="flex items-center gap-3 mt-2">
+              <IoTimeOutline />
+              <span className="bg-gray-300 rounded-md px-4 ">
+                {processing_time}
+              </span>
             </div>
-            <div className="flex space-x-4 mb-6 text-sm font-medium">
-              <div className="flex-auto flex space-x-4">
-                <button
-                  className="h-10 px-6 font-semibold rounded-md border border-balck-800 text-teal-900 bg-teal-300"
-                  type="button"
-                >
-                  Update
-                </button>
-                <button
-                  className="h-10 px-6 font-semibold rounded-md border border-balck-800 text-red-900 bg-red-300"
-                  type="button"
-                >
-                  Delete
-                </button>
-              </div>
+          </div>
+          <div className="flex space-x-4 mb-6 text-sm font-medium">
+            <div className="flex-auto flex space-x-4">
               <button
-                className="flex items-center justify-center gap-2 w-14 h-9 rounded-md text-gray-800 border border-slate-500 shadow-2xl"
+                className="h-10 px-6 font-semibold rounded-md border border-black-800 text-teal-900 bg-teal-300 hover:bg-transparent hover:font-bold"
                 type="button"
-                aria-label="Favorites"
               >
-                <span className="text-lg font-semibold">{rating}</span>
-                <IoIosStar />
+                Update
+              </button>
+              <button
+                className="h-10 px-6 font-semibold rounded-md border border-black-800 text-red-900 bg-red-300 hover:bg-transparent hover:font-bold"
+                type="button"
+              >
+                Delete
               </button>
             </div>
-            <div className="flex  items-center gap-2">
-              <MdOutlineDesignServices />
-              <p className="text-sm  text-slate-700 ">
-                Customization: <span className="font-bold">{customization}</span>
-              </p>
-            </div>
-          </form>
-        </div>
+            <button
+              className="flex items-center justify-center gap-2 w-14 h-9 rounded-md text-gray-800 border border-slate-500 shadow-2xl"
+              type="button"
+              aria-label="Favorites"
+            >
+              <span className="text-lg font-semibold ">{rating}</span>
+              <IoIosStar className="text-orange-500" />
+            </button>
+          </div>
+          <div className="flex items-center gap-2">
+            <MdOutlineDesignServices />
+            <p className="text-sm text-slate-700 ">
+              Customization: <span className="font-bold">{customization}</span>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
 };
 
 export default MyCraftCard;
+
+MyCraftCard.propTypes = {
+  craft: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    category: PropTypes.string.isRequired,
+    craftPhotoURL: PropTypes.string.isRequired,
+    processing_time: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    stockStatus: PropTypes.string.isRequired,
+    customization: PropTypes.string.isRequired,
+  }).isRequired,
+};
--- src/pages/MyCraft/MyCraft.jsx
+++ src/pages/MyCraft/MyCraft.jsx
@@ -2,28 +2,33 @@ import { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
 import MyCraftCard from "../../components/MyCraftCard/MyCraftCard";
 
-
 const MyCraft = () => {
-    const {user} = useAuth() || {};
-    const [items, setItems] = useState([]);
+  const { user } = useAuth() || {};
+  const [items, setItems] = useState([]);
 
-    useEffect(() => {
-        fetch(`http://localhost:5000/craft/${user?.email}`) 
-        .then(res => res.json())
-        .then(data => {
-            setItems(data)
-        })
-    }, [user])
+  useEffect(() => {
+    fetch(`http://localhost:5000/craft/${user?.email}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setItems(data);
+      });
+  }, [user]);
 
-    console.log(items);
+  console.log(items);
 
-    return (
-        <div className="">
-            {
-                items?.map(craft => <MyCraftCard craft={craft} key={craft._id}></MyCraftCard>)
-            }
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 place-items-center gap-6 max-w-screen-xl mx-auto">
+      {items.length > 0 ? (
+        items?.map((craft) => (
+          <MyCraftCard craft={craft} key={craft._id}></MyCraftCard>
+        ))
+      ) : (
+        <div className="flex justify-center items-center min-h-[calc(100vh-383px)] w-full">
+          <p className="text-6xl font-bold">Opps! No Data Found</p>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default MyCraft;
